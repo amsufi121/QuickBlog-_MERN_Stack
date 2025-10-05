15924d368a3d06b4e5dd4ed6319e7d13864b85f6
Sufiyan <csesufi@gmail.com>
2025-10-05 23:34:47 +0530

    home page all done, bloglist, blogcard added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import BlogList from "../components/BlogList";
+import BlogCard from "../components/BlogCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <BlogList />
+      <BlogCard />
+    </>
+  );
 };
 
 export default Home;
